c89e8988c2bc9a655eca90fdfed15d36bff85862
marcusthesage <114116426+marcusthesage@users.noreply.github.com>
2023-12-05 22:48:28 -0600

    12-5-23 pt2
    alpha officially underway

--- index.js
+++ index.js
@@ -1,173 +1,34 @@
+
+// Constants and Variables
 const sandwich = {
-    sandwichBread: {
-        bread1: {
-            bread1name:"Sour Dough",
-            bread1score:0 ,
-        } ,
-        bread2: {
-            bread2name:"Marble Rye",
-            bread2score:0 ,
-        } ,
-        bread3:{
-            bread3name:"Ciabata Bun",
-            bread3score:0 ,
-        },
-        bread4:{
-            bread4name:"Honey Wheat",
-            bread4score:0 ,
-        },
-        bread5:{
-            bread5name:"Croissant",
-            bread5score:0 ,
-        },
-    } ,  
-    sandwichMeat: {
-        meat1: {
-            meat1name:"Ham",
-            meat1score:0 ,
-        } ,
-        meat2: {
-            meat2name:"Turkey",
-            meat2score:0 ,
-        } ,
-        meat3:{
-            meat3name:"Chicken",
-            meat3score:0 ,
-        },
-        meat4:{
-            meat4name:"Pepperoni and Salami",
-            meat4score:0 ,
-        },
-        meat5:{
-            meat5name:"Roast Beef",
-            meat5score:0 ,
-        },
-        meat6:{
-            meat6name:"Veggie",
-            meat6score:0 ,
-        },
-    } ,
-    sandwichCheese: {
-        cheese1: {
-            cheese1name:"Cheddar",
-            cheese1score:0 ,
-        } ,
-        cheese2: {
-            cheese2name:"Swiss",
-            cheese2score:0 ,
-        } ,
-        cheese3:{
-            cheese3name:"Provolone",
-            cheese3score:0 ,
-        },
-        cheese4:{
-            cheese4name:"Pepperjack",
-            cheese4score:0 ,
-        },
-        cheese5:{
-            cheese5name:"American Cheese",
-            cheese5score:0 ,
-        },
-        cheese6:{
-            cheese6name:"Gouda",
-            cheese6score:0 ,
-        },
-
-    } ,
-    sandwichToasted: {
-        toasted1: {
-            toasted1name:"Toasted",
-            toasted1score:0 ,
-        } ,
-        toasted2: {
-            toasted2name:"Cold",
-            toasted2score:0 ,
-        } ,
-    } ,
-    sandwichCondiments: {
-        condiments1: {
-            condiments1name:"Mayo",
-            condiments1score:0 ,
-        } ,
-        condiments2: {
-            condiments2name:"Mustard",
-            condiments2score:0 ,
-        } ,
-        condiments3:{
-            condiments3name:"BBQ Sauce",
-            condiments3score:0 ,
-        },
-        condiments4:{
-            condiments4name:"Chipotle Sauce",
-            condiments4score:0 ,
-        },
-        condiments5:{
-            condiments5name:"Hot Sauce",
-            condiments5score:0 ,
-        },
-        condiments6:{
-            condiments6name:"Horseraddish",
-            condiments6score:0 ,
-        },
-        condiments7:{
-            condiments7name:"Ranch",
-            condiments7score:0 ,
-        },
-        condiments8:{
-            condiments8name:"Salt and Pepper",
-            condiments8score:0 ,
-        },
-    } ,
-    sandwichToppings: {
-        toppings1: {
-            toppings1name:"Lettuce",
-            toppings1score:0 ,
-        } ,
-        toppings2: {
-            toppings2name:"Tomato",
-            toppings2score:0 ,
-        } ,
-        toppings3:{
-            toppings3name:"Onions",
-            toppings3score:0 ,
-        },
-        toppings4:{
-            toppings4name:"Cucumbers",
-            toppings4score:0 ,
-        },
-        toppings5:{
-            toppings5name:"Banana Peppersh",
-            toppings5score:0 ,
-        },
-        toppings6:{
-            toppings6name:"Jalapeños",
-            toppings6score:0 ,
-        },
-        toppings7:{
-            toppings7name:"Pickles",
-            toppings7score:0 ,
-        },
-
-    } ,
-
-
+    bread: "",
+    meat: "",
+    cheese: "",
+    toasted: "",
+    condiments: [],
+    vegetables: []
 }
+const specialSandwich = {
+    bread: "Ciabatta Bun",
+    meat: "Pepperoni and Salami",
+    cheese: 'Provolone',
+    toasted: "Toasted",
+    condiments: ["Ranch", "Mustard"],
+    vegetables: ["Lettuce", "Tomato", "Onions", "Cucumbers", "Banana Peppers", "Jalapeños", "Pickles"]
+};
+var options = {
 
-let selectedBread = '';
-let selectedMeat = '';
-let selectedCheese = '';
-let selectedToasted = '';
-let selectedCondiments = '';
-let selectedVegetables = '';
+    'Bread': ['Sour Dough', 'Ciabatta Bun', 'Marble Rye', 'Honey Wheat', 'Croissant'],
+    'Meat': ['Turkey', 'Ham', 'Chicken', 'Pepperoni and Salami', 'Roast Beef', 'Veggie'],
+    'Cheese': ['Provolone', 'Cheddar', 'Havarti', 'American Cheese', 'Swiss', 'Pepperjack', 'Gouda'], 
+    'Toasted': ['Toasted', 'Cold', ],
+    'Condiments': ['Ranch', 'Mayonaise', 'Hot Sauce', 'BBQ Sauce', 'Chipotle Mayo', 'Horseraddish', 'Mustard', 'Salt and Pepper'],
+    'Vegetables': ['Lettuce', 'Tomato', 'Onions', 'Cucumbers', 'Banana Peppers', 'Jalapeños', 'Pickles'],
+    // Add the options for the other ingredients here
+};
+var savedSandwiches = [];
 
-
-// Select the <p> tag with the "Choice" id and hide it
-document.getElementById('Choice').style.display = 'none';
-
-
-
-
-
+// Functions
 function selectOption(option, ingredientType, action) {
     // Get the list item for the specified ingredient type
     var listItem = document.getElementById(ingredientType.toLowerCase() + 'List');
@@ -186,7 +47,7 @@ function selectOption(option, ingredientType, action) {
             var options = listItem.firstElementChild.textContent.split(', ');
             if (options.indexOf(option) === -1) { // Check if the option is already in the list
                 if (listItem.firstElementChild.textContent !== ingredientType) {
-                    listItem.firstElementChild.textContent += ', ' + option;
+                    listItem.firstElementChild.textContent += ' + ' + option;
                 } else {
                     listItem.firstElementChild.textContent = option;
                 }
@@ -196,7 +57,28 @@ function selectOption(option, ingredientType, action) {
             listItem.firstElementChild.textContent = option;
         }
     }
-
+    if (ingredientType === 'Bread') {
+        sandwich.bread = option;
+    }
+    if (ingredientType === 'Meat') {
+        sandwich.meat = option;
+    }
+    if (ingredientType === 'Cheese') {
+        sandwich.cheese = option;
+    }
+    if (ingredientType === 'Toasted') {
+        sandwich.toasted = option;
+    }
+    if (ingredientType === 'Condiments') {
+        if (!sandwich.condiments.includes(option)) {
+            sandwich.condiments.push(option);
+        }
+    }
+    if (ingredientType === 'Vegetables') {
+        if (!sandwich.vegetables.includes(option)) {
+            sandwich.vegetables.push(option);
+        }
+    }
     // Get all the option buttons
     var optionButtons = document.getElementsByClassName('choices');
 
@@ -211,17 +93,6 @@ function selectOption(option, ingredientType, action) {
         }
     }
 }
-// Define the options for each ingredient
-var options = {
-    'Bread': ['Sour Dough', 'Ciabata Bun', 'Marble Rye', 'Honey Wheat', 'Croissant'],
-    'Meat': ['Turkey', 'Ham', 'Chicken', 'Pepperoni and Salami', 'Roast Beef', 'Veggie'],
-    'Cheese': ['Provolone', 'Cheddar', 'Havarti', 'American Cheese', 'Swiss', 'Pepperjack', 'Gouda'], 
-    'Toasted': ['Toasted', 'Cold', ],
-    'Condiments': ['Ranch', 'Mayonaise', 'Hot Sauce', 'BBQ Sauce', 'Chipotle Mayo', 'Horseraddish', 'Mustard', 'Salt and Pepper'],
-    'Vegetables': ['Lettuce', 'Tomato', 'Onions', 'Cucumbers', 'Banana Peppers', 'Jalapeños', 'Pickles'],
-    // Add the options for the other ingredients here
-};
-
 function selectIngredient(ingredientType) {
     // Get the 'Choice' paragraph
     var choiceParagraph = document.getElementById('Choice');
@@ -274,13 +145,11 @@ function selectIngredient(ingredientType) {
     }
     document.getElementById('Choice').style.display = 'block';
 }
-
-
 function updateIngredientLists() {
   const breadList = document.getElementById('breadList');
   const meatList = document.getElementById('meatList');
 
-  // Clear existing content
+  
   
   // Update content based on selected ingredients
   if (selectedBread) {
@@ -295,18 +164,6 @@ function updateIngredientLists() {
     meatList.appendChild(meatItem);
   }
 }
-
-// Get the "store" div
-var storeDiv = document.getElementById('store');
-
-// Create a 'Reset' button
-var resetButton = document.createElement('button');
-resetButton.textContent = 'Reset';
-resetButton.onclick = resetSandwich;
-
-// Add the 'Reset' button to the "store" div
-storeDiv.appendChild(resetButton);
-
 function resetSandwich() {
     // Define the ingredient types
     var ingredientTypes = ['Bread', 'Meat', 'Cheese', 'Toasted', 'Condiments', 'Vegetables'];
@@ -329,40 +186,6 @@ function resetSandwich() {
         optionButtons[i].classList.remove('selected');
     }
 }
-
-// Create an array to store the saved sandwiches
-var savedSandwiches = [];
-
-// Create an input field for the sandwich name
-var sandwichNameInput = document.createElement('input');
-sandwichNameInput.id = 'sandwichName';
-
-// Create a 'Save' button
-var saveButton = document.createElement('button');
-saveButton.textContent = 'Save';
-saveButton.onclick = function() {
-    // Get the sandwich name from the input field
-    var sandwichName = document.getElementById('sandwichName').value;
-
-    // Get the current sandwich from the `sandwich` object
-    var currentSandwich = sandwich;
-
-    // Save the sandwich name and sandwich to the `savedSandwiches` array
-    savedSandwiches.push({ name: sandwichName, sandwich: currentSandwich });
-
-    // Optionally, display the saved sandwiches in the UI
-    var savedSandwichesList = document.getElementById('savedSandwichesList');
-    var listItem = document.createElement('li');
-    listItem.textContent = sandwichName;
-    savedSandwichesList.appendChild(listItem);
-};
-
-// Add the input field and 'Save' button to the document
-storeDiv.appendChild(sandwichNameInput);
-storeDiv.appendChild(saveButton);
-  //console.log(breadChoice)
-
-
 function updateSavedSandwichesList() {
     // Get the saved sandwiches list
     var savedSandwichesList = document.getElementById('savedSandwichesList');
@@ -382,6 +205,41 @@ function updateSavedSandwichesList() {
         savedSandwichesList.appendChild(listItem);
     }
 }
+function isSpecialSandwich() {
+    if (sandwich.bread !== specialSandwich.bread ||
+        sandwich.meat !== specialSandwich.meat ||
+        sandwich.cheese !== specialSandwich.cheese ||
+        sandwich.toasted !== specialSandwich.toasted) {
+        return false;
+    }
+
+    if (!arraysContainSameElements(sandwich.condiments, specialSandwich.condiments) ||
+        !arraysContainSameElements(sandwich.vegetables, specialSandwich.vegetables)) {
+        return false;
+    }
+
+    return true;
+}
+
+function arraysContainSameElements(array1, array2) {
+    if (array1.length !== array2.length) {
+        return false;
+    }
+
+    array1.sort();
+    array2.sort();
+
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+// Event Handlers and Function Calls
+// Select the <p> tag with the "Choice" id and hide it
+document.getElementById('Choice').style.display = 'none';
 
 
 
@@ -391,4 +249,78 @@ function updateSavedSandwichesList() {
 
 
 
-//document.getElementById("Score").innerHTML = sandwich.sandwichBread.bread5.bread5name;
+
+
+
+
+// Define the options for each ingredient
+
+// Get the "store" div
+var storeDiv = document.getElementById('store');
+
+// Create a 'Reset' button
+var resetButton = document.createElement('button');
+resetButton.textContent = 'Reset';
+resetButton.onclick = resetSandwich;
+
+// Add the 'Reset' button to the "store" div
+storeDiv.appendChild(resetButton);
+
+
+
+
+
+
+// Create an input field for the sandwich name
+var sandwichNameInput = document.createElement('input');
+sandwichNameInput.id = 'sandwichName';
+storeDiv.appendChild(sandwichNameInput);
+// Create a 'Save' button
+var saveButton = document.createElement('button');
+saveButton.textContent = 'Save';
+saveButton.onclick = function() {
+    // Get the sandwich name from the input field
+    var sandwichName = document.getElementById('sandwichName').value;
+
+    // Get the current sandwich from the `sandwich` object
+    var currentSandwich = sandwich;
+
+    // Save the sandwich name and sandwich to the `savedSandwiches` array
+    savedSandwiches.push({ name: sandwichName, sandwich: currentSandwich });
+
+    // Optionally, display the saved sandwiches in the UI
+    var savedSandwichesList = document.getElementById('savedSandwichesList');
+    var listItem = document.createElement('li');
+    listItem.textContent = sandwichName;
+    
+    console.log(sandwichName, currentSandwich);
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(currentSandwich));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", sandwichName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+    var fishermanParagraph = document.getElementById('Fisherman');
+    fishermanParagraph.textContent = "You've made a sandwich named " + sandwichName + "!";
+};
+storeDiv.appendChild(saveButton);
+storeDiv.style.display = 'flex';
+storeDiv.style.justifyContent = 'center';
+storeDiv.style.alignItems = 'center';
+storeDiv.style.flexDirection = 'column';
+if (isSpecialSandwich()) {
+    console.log("You've made the special sandwich!");
+}
+
+
+// Total Combinations
+var breadOptions = options['Bread'].length;
+var meatOptions = options['Meat'].length;
+var cheeseOptions = options['Cheese'].length;
+var toastedOptions = options['Toasted'].length;
+var condimentOptions = Math.pow(2, options['Condiments'].length);
+var vegetableOptions = Math.pow(2, options['Vegetables'].length);
+
+var totalCombinations = breadOptions * meatOptions * cheeseOptions * toastedOptions * condimentOptions * vegetableOptions;
+//console.log(totalCombinations);
